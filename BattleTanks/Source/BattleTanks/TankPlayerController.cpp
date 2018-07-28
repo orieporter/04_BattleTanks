9a37133bb7bba966e0c63e6f873b99cf968cfaa8
Orie <orieporter@gmail.com>
2018-07-27 21:41:27 -0500

    Lecture 127 - Using virtual and override

--- BattleTanks/Source/BattleTanks/TankPlayerController.cpp
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -3,7 +3,19 @@
 #include "TankPlayerController.h"
 
 
-
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controlling %s"), *ControlledTank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Tank Not Valid"))
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
--- BattleTanks/Source/BattleTanks/TankPlayerController.h
+++ BattleTanks/Source/BattleTanks/TankPlayerController.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "Tank.h"
+#include "Public/Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
@@ -17,6 +17,8 @@ class BATTLETANKS_API ATankPlayerController : public APlayerController
 	
 public:
 	ATank * GetControlledTank() const;
+
+	virtual void BeginPlay() override;
 	
 	
 };
