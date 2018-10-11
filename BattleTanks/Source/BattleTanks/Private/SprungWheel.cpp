2aae41f798b5b6781abe03e7d14662429f4ad841
Orie <orieporter@gmail.com>
2018-10-11 10:46:29 -0500

    Lecture 217 - Attachments, Actors, and Components

--- BattleTanks/Source/BattleTanks/Private/SprungWheel.cpp
+++ BattleTanks/Source/BattleTanks/Private/SprungWheel.cpp
@@ -11,21 +11,29 @@ ASprungWheel::ASprungWheel()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Physics Constraint"));
+	SetRootComponent(PhysicsConstraint);
+
 	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
-	SetRootComponent(Mass);
+	Mass->SetupAttachment(RootComponent);
 
 	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
 	Wheel->SetupAttachment(RootComponent);
-
-	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Physics Constraint"));
-	PhysicsConstraint->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (GetAttachParentActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Begin not null"), *GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Begin null"), *GetName());
+	}
 }
 
 // Called every frame
@@ -33,5 +41,12 @@ void ASprungWheel::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (GetAttachParentActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Not null"), *GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s: Null"), *GetName());
+	}
 }
-
